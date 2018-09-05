e16d9664d2ab8a30d4188bb1a0d2f0120d0a8b38
vec <vec@torzo.club>
2018-09-05 22:38:48 +0800

    add unit test

---
+++ test/is-first.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+import '../src/main'
+
+test(`isFirst'`, t => {
+  const arr = [2, 3, 1, 7, 9]
+  t.true(arr.isFirst(2))
+})
+
+test(`isFirstBy`, t => {
+  const [a, b, c] = [{ id: 9 }, { id: 10 }, { id: 11 }]
+  const arr = [a, b, c]
+
+  t.true(arr.isFirstBy('id', 9))
+  t.false(arr.isFirstBy('id', 10))
+  t.false(arr.isFirstBy('id', 11))
+  t.false(arr.isFirstBy('id', 0))
+})
+
+test('isFirstByMatch', t => {
+  const [a, b, c] = [{ id: 9 }, { id: 10 }, { id: 11 }]
+  const arr = [a, b, c]
+
+  t.true(arr.isFirstByMatch({ id: 9 }))
+  t.false(arr.isFirstByMatch({ id: 10 }))
+  t.false(arr.isFirstByMatch({ id: 11 }))
+  t.false(arr.isFirstByMatch({ id: 0 }))
+})
---
+++ test/is-last.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+import '../src/main'
+
+test(`isLast'`, t => {
+  const arr = [2, 3, 1, 7, 9]
+  t.true(arr.isLast(9))
+})
+
+test(`isLastBy`, t => {
+  const [a, b, c] = [{ id: 9 }, { id: 10 }, { id: 11 }]
+  const arr = [a, b, c]
+
+  t.false(arr.isLastBy('id', 0))
+  t.false(arr.isLastBy('id', 9))
+  t.false(arr.isLastBy('id', 10))
+  t.true(arr.isLastBy('id', 11))
+})
+
+test(`isLastByMatch`, t => {
+  const [a, b, c] = [{ id: 9 }, { id: 10 }, { id: 11 }]
+  const arr = [a, b, c]
+
+  t.false(arr.isLastByMatch({ id: 0 }))
+  t.false(arr.isLastByMatch({ id: 9 }))
+  t.false(arr.isLastByMatch({ id: 10 }))
+  t.true(arr.isLastByMatch({ id: 11 }))
+})
